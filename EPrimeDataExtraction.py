1560b91aadb84324009d9c48e33edc825b5c38e6
cwalsh27 <146851951+cwalsh27@users.noreply.github.com>
2024-01-18 13:03:49 -0500

    Initial upload of fully functional script

---
+++ EPrimeDataExtraction.py
@@ -0,0 +1,72 @@
+from openpyxl import *
+del open
+
+
+# Load row numbers of selected trials
+wb = load_workbook("input_data.xlsx")
+ws = wb.active
+
+goodTrials = [(1, 0)]  # list of row numbers for button press trials, includes header row
+onsetTimes = []
+vowelCol = 0
+for col in range(1, 66):  # 66 is just three iterations of the alphabet, number is arbitrary
+    if ws.cell(1, col).value == 'TaskPresentation.RESP':
+        for row in range(2, 80):  # 80 to account for extra rows, 2 to skip line header
+            if str(ws.cell(row, col).value) != "None":
+                goodTrials.append((row, ws.cell(row, col+1).value))
+    if ws.cell(1, col).value == 'TaskPresentation.OnsetTime':
+        for row in range(1, 80):
+            onsetTimes.append(ws.cell(row, col).value)
+    if ws.cell(1, col).value == 'vowels':
+        vowelCol = col
+
+
+# new sheet
+ws2 = wb.create_sheet('refined_data.xlsx')
+ws3 = wb.create_sheet('trial_start_times.xlsx')
+
+goodVowels = []
+
+# populate refined_data
+row = 1
+for goodTrial in goodTrials:
+    for col in range(1, 66):
+        if (goodTrial[1] > 0) and (goodTrial[1] < 3000):
+            testVal = ws.cell(goodTrial[0]-1, col).value
+            ws2.cell(row, col).value = testVal
+    if goodTrial[0] > 1:
+        goodVowels.append(ws.cell(goodTrial[0]-1, vowelCol).value)
+    row += 1
+
+# populate trial_start_times
+for i in range(1, len(onsetTimes)):
+    if onsetTimes[i]:
+        ws3.cell(i, 1).value = str(onsetTimes[i])
+
+# calculate vowel biases
+vowelString = ""
+for vowels in goodVowels:
+    vowelString += vowels
+
+EaCount = vowelString.count("æ -ɛ - e - ɪ - i - ɪ- e - ɛ- æ")
+OuCount = vowelString.count("a - Ɔ - o - Ʊ – u – Ʊ – o – Ɔ – a")
+AaCount = vowelString.count("A / A")
+vowelCounts = [EaCount, OuCount, AaCount]
+
+row += 5
+ws2.cell(row, 1).value = "æ -ɛ - e - ɪ - i - ɪ- e - ɛ- æ"
+ws2.cell(row+1, 1).value = "a - Ɔ - o - Ʊ – u – Ʊ – o – Ɔ – a"
+ws2.cell(row+2, 1).value = "A / A"
+
+for i in range(0, 3):
+    ws2.cell(row+i, 2).value = vowelCounts[i]
+
+# save spreadsheet changes
+wb.save("output_data.xlsx")
+
+
+# create/populate vowelBias text file
+vowelFile = open("vowelBias.txt", "w")
+vowelFile.write("Ea Count: " + str(EaCount) + "\n")
+vowelFile.write("Ou Count: " + str(OuCount) + "\n")
+vowelFile.write("A/A Count: " + str(AaCount) + "\n")
