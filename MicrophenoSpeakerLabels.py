d53b4f83a726f71d76e47b7e786c9df6c1085254
Coleman <cowalsh@udel.edu>
2024-04-17 18:08:43 -0400

    first draft of self-authored doc-reader, finding bold text and labeling paragraphs according to speaker shift

--- MicrophenoSpeakerLabels.py
+++ MicrophenoSpeakerLabels.py
@@ -1,29 +1,26 @@
+import docx
 import os
+from docx import Document
 
-text_dir = r'TextFiles'
-text_file = os.path.join(text_dir, 'TextFiles/CSSPRING20230512-181729_Recording_1280x720.docx')
+desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
+doc_path = os.path.join(desktop_path, r'TextFiles/CSSPRING20230512-181729_Recording_1280x720_MM.docx')
 
-def add_line_numbers(filename):
-    try:
-        # Open the file in read mode
-        with open(filename, 'r') as file:
-            # Read the content of the file
-            lines = file.readlines()
+doc = Document(doc_path)
 
-        # Open the file in write mode to overwrite the content with line numbers
-        with open(filename, 'w') as file:
-            # Iterate over each line and add line numbers
-            for i, line in enumerate(lines, start=1):
-                if i % 2 == 1:
-                    file.write(f"B{i}: {line}")
-                else:
-                    file.write(f"A{i}: {line}")
+print('List of paragraphs')
+for para in doc.paragraphs:
+    print(para.text, "\n\n\n")
 
 
-        print("Line numbers added successfully.")
-    except FileNotFoundError:
-        print("File not found.")
+last_speaker_B = True
 
-# Example usage
-filename = 'example.txt'  # Replace 'example.txt' with your file name
-add_line_numbers('TextFiles/CSSPRING20230512-181729_Recording_1280x720.txt')
+for count, para in enumerate(doc.paragraphs):
+    if any(run.bold for run in para.runs):
+        if not last_speaker_B:
+            print("B: " + para.text)
+            last_speaker_B = True
+    else:
+        if last_speaker_B:
+            print("A: " + para.text)
+            last_speaker_B_ = False
+
