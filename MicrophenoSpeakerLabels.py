f0a7a64b2acdbfc8fe2d59c93f04b50a41eb704e
Coleman <cowalsh@udel.edu>
2024-04-18 00:49:28 -0400

    functional bold filter, writes new file with modified line names. struggles with interjections and trailing bold whitespace

--- MicrophenoSpeakerLabels.py
+++ MicrophenoSpeakerLabels.py
@@ -1,26 +1,43 @@
-import docx
 import os
 from docx import Document
 
 desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
 doc_path = os.path.join(desktop_path, r'TextFiles/CSSPRING20230512-181729_Recording_1280x720_MM.docx')
+new_doc_path = os.path.join(desktop_path, r'TextFiles/outputFile')
 
 doc = Document(doc_path)
+newDoc = Document()
 
-print('List of paragraphs')
+# Text processing
+# def strip_trailing_spaces(text):
+#     return '\n'.join(line.rstrip() for line in text.split('\n'))
+#
+# for paragraph in doc.paragraphs:
+#     paragraph.text = strip_trailing_spaces(paragraph.text)
+
+# appending line labels
+last_speaker_B = False
+lines = []
+count = 0
+
 for para in doc.paragraphs:
-    print(para.text, "\n\n\n")
-
-
-last_speaker_B = True
-
-for count, para in enumerate(doc.paragraphs):
-    if any(run.bold for run in para.runs):
-        if not last_speaker_B:
-            print("B: " + para.text)
-            last_speaker_B = True
+    newP = newDoc.add_paragraph('')
+    if para.text[0:2] == "((" and para.text[-2:] == "))":
+        newP.add_run(para.text)
     else:
-        if last_speaker_B:
-            print("A: " + para.text)
-            last_speaker_B_ = False
+        if any(run.bold for run in para.runs) and not last_speaker_B:
+            newP.add_run(f'B{count+1}: {para.text}').bold = True
+            last_speaker_B = True
+            count += 1
+        elif any(run.bold for run in para.runs) and last_speaker_B:
+            newP.add_run(para.text).bold = True
+            count += 1
+        elif last_speaker_B:
+            newP.add_run(f'A{count + 1}: {para.text}')
+            last_speaker_B = False
+            count += 1
+        else:
+            newP.add_run(para.text)
+            count += 1
 
+newDoc.save('outputFile.docx')
